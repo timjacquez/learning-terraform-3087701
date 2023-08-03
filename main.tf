92b8105888fd26ca0c217cbff61a31fb86165908
Tim Jacquez <timjacquez@gmail.com>
2023-08-03 15:28:55 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,8 @@ data "aws_ami" "app_ami" {
 }
 
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws" version = 3.14.2
+  source = "terraform-aws-modules/vpc/aws" 
+  version = 3.14.2
 
   name = "dev"
   cidr = "10.0.0.0/16"
