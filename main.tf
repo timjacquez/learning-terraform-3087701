f42532fc82b9e85181d32c18c9c558f5df3f2998
Tim Jacquez <timjacquez@gmail.com>
2023-08-03 14:36:36 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,7 +33,9 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+  subnet_id              = module.vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
+  
 
   tags = {
     Name = "HelloWorld"
