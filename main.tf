5ed8d89de8a216c5347936dffa67569245323734
Tim Jacquez <timjacquez@gmail.com>
2023-08-03 15:58:55 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ module "blog-alb" {
 
   vpc_id             = module.vpc.vpc_id
   subnets            = module.vpc.public_subnets
-  security_groups    = "${module.blog_sg.security_group_id}"
+  security_groups    = ["${module.blog_sg.security_group_id}"]
 
   target_groups = [
     {
