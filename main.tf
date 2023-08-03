1193a013179e7461a6e1aaf870a1da0316302ee0
Tim Jacquez <timjacquez@gmail.com>
2023-08-03 14:32:29 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -45,7 +45,8 @@ module "blog_sg" {
   version = "4.13.0"
   name    = "blog"
   
-  vpc_id = module.blog_vpc.vpc_id
+  vpc_id = module.vpc.vpc_id
+  
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
