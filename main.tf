a5e9650705fa512d782d9b589ee9e357c4704e7c
Tim Jacquez <timjacquez@gmail.com>
2023-08-03 14:27:07 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-module "vpc" {
+module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
@@ -45,7 +45,7 @@ module "blog_sg" {
   version = "4.13.0"
   name    = "blog"
   
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id = module.blog_vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
