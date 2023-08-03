15732e57474626f2043f6a8913f71b6d9f55f747
Tim Jacquez <timjacquez@gmail.com>
2023-08-03 16:33:24 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,24 +29,27 @@ module "vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
 
-  subnet_id              = module.vpc.public_subnets[0]
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-  
+  name = "blog"
+  min_size = 1
+  max_size = 2
 
-  tags = {
-    Name = "HelloWorld"
-  }
+  vpc_zone_identifier = module.vpc.public_subnets[0]
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+    
+  image_id            = data.aws_ami.app_ami.id
+  instance_type       = var.instance_type
 }
 
-module "blog-alb" {
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
-  name = "blog-alb"
+  name = "blog_alb"
 
   load_balancer_type = "application"
 
