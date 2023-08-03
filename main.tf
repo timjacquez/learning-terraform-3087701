816ee9cc721c9ae13d91fd773f47d017fed81ecc
Tim Jacquez <timjacquez@gmail.com>
2023-08-03 15:24:02 -0600

    Update main.tf
    load balancer

--- main.tf
+++ main.tf
@@ -42,6 +42,50 @@ resource "aws_instance" "blog" {
   }
 }
 
+module "blog-alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 8.0"
+
+  name = "blog-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.vpc.vpc_id
+  subnets            = module.vpc.public_subnets
+  security_groups    = module.blog_sg.security_group_id
+
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
+        my_other_target = {
+          target_id = "i-a1b2c3d4e5f6g7h8i"
+          port = 8080
+        }
+      }
+    }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
