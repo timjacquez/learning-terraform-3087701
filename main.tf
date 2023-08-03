aaeeb49da48b819f935614dcbeb5ceb4ee31843e
Tim Jacquez <timjacquez@gmail.com>
2023-08-03 14:20:07 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,8 +23,6 @@ module "vpc" {
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = false
-
   tags = {
     Terraform = "true"
     Environment = "dev"
@@ -44,7 +42,7 @@ resource "aws_instance" "blog" {
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.0"
+  version = "4.13.0"
   name    = "blog"
   
   vpc_id = module.vpc.public_subnets[0]
